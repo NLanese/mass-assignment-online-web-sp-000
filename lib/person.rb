74314f540e3a00244bfcce910309987f0463c569
Nick Lanese <nlanese21@gmail.com>
2020-08-27 07:18:41 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  attr_accessor :name, :birthday, :eve_color, :height, :weight, :handed, :hair_color,
+  attr_accessor :name, :birthday, :eye_color, :height, :weight, :handed, :hair_color,
                 :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
