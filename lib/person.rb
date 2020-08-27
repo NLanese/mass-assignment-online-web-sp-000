30673cc633c92e0c34946302ba018911f305078a
Nick Lanese <nlanese21@gmail.com>
2020-08-27 07:16:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,21 +2,8 @@ class Person
   attr_accessor :name, :birthday, :evey_color, :height, :weight, :handed,
                 :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(name:, birthday:, eye_color:, height:,
-                 weight:, handed:, complexion:, t_shirt_size:,
-                 wrist_size:, glove_size:, pant_length:, pant_width:)
-    @name = name
-    @birthday = birthday
-    @eye_color = eye_color
-    @height = height
-    @weight = weight
-    @handed = handed
-    @complexion = complexion
-    @t_shirt_size = t_shirt_size
-    @wrist_size = wrist_size
-    @glove_size = glove size
-    @pant_length = pant_length
-    @pant_width = pant_width
+  def initialize(attributes)
+    attributes.each {|key, value| self.send (("#{key}="), value)}
   end
 
 
